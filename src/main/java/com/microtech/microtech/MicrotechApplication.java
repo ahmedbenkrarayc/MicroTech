6819904964cfe64a5cd0f614a82df81eff24df96
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-25 17:05:16 +0100

    init project

---
+++ src/main/java/com/microtech/microtech/MicrotechApplication.java
@@ -0,0 +1,13 @@
+package com.microtech.microtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microtech/microtech/MicrotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microtech.microtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
