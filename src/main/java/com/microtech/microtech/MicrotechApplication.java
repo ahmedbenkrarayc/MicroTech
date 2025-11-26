179e7607968dadd195e059e0b774db2115b5ed90
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-26 10:18:35 +0100

    MIC-55 Configure application properties

--- src/main/java/com/microtech/microtech/MicrotechApplication.java
+++ src/main/java/com/microtech/microtech/MicrotechApplication.java
@@ -1,5 +1,6 @@
 package com.microtech.microtech;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicrotechApplication {
 
 	public static void main(String[] args) {
+		loadEnv();
 		SpringApplication.run(MicrotechApplication.class, args);
 	}
 
+	private static void loadEnv(){
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			if(entry.getKey().equalsIgnoreCase("ACTIVE_PROFILE")){
+				var dotenv1 = Dotenv.configure().filename(".env."+entry.getValue()).ignoreIfMissing().load();
+				dotenv1.entries().forEach(entry2 -> {
+					System.setProperty(entry2.getKey(), entry2.getValue());
+				});
+			}
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+	}
 }
