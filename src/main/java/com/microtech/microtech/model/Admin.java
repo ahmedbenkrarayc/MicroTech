869854542b6210aa1f87cdecf9d207c1ae87d678
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-28 11:56:46 +0100

    MIC-76 Create Admin Entity

---
+++ src/main/java/com/microtech/microtech/model/Admin.java
@@ -0,0 +1,17 @@
+package com.microtech.microtech.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("client")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Admin extends User{
+}
