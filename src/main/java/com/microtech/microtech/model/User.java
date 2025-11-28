7c68ff1ca17be11867eaefae02a55895e84c621d
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-11-28 11:50:30 +0100

    MIC-74 Create User Entity

---
+++ src/main/java/com/microtech/microtech/model/User.java
@@ -0,0 +1,50 @@
+package com.microtech.microtech.model;
+
+import com.microtech.microtech.model.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "type")
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @Column(nullable = false)
+    private String fname;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @Column(nullable = false)
+    private String lname;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email format is invalid")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    @Column(nullable = false)
+    private String password;
+
+    @NotNull(message = "Role is required")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
