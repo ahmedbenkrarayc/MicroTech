c8dd6325ed7fa5d90879663d933736e0004bf5fd
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-05 00:27:55 +0100

    MIC-69 Created interceptor for verifying roles by action

---
+++ src/main/java/com/microtech/microtech/security/interceptor/RoleInterceptor.java
@@ -0,0 +1,49 @@
+package com.microtech.microtech.security.interceptor;
+
+import com.microtech.microtech.security.annotation.RolesAllowed;
+import com.microtech.microtech.security.exception.ForbiddenException;
+import com.microtech.microtech.security.exception.UnauthorizedException;
+import com.microtech.microtech.security.service.SessionService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class RoleInterceptor implements HandlerInterceptor {
+    private final SessionService sessionService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler
+    ) {
+        if(!(handler instanceof HandlerMethod handlerMethod)){
+            return true;
+        }
+
+        RolesAllowed annotation = handlerMethod.getMethodAnnotation(RolesAllowed.class);
+        if(annotation == null){
+            return true;
+        }
+
+        String currentUserRole = sessionService.getUserRole();
+        if(currentUserRole == null){
+            throw new UnauthorizedException();
+        }
+
+        boolean allowed = Arrays.stream(annotation.roles())
+                .anyMatch(role -> role.equalsIgnoreCase(currentUserRole));
+
+        if(!allowed){
+            throw new ForbiddenException();
+        }
+
+        return true;
+    }
+}
