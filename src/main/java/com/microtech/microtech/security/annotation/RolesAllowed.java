f06dd7e3bec79dc08d16a0c28d3c5f3c7a3c3770
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-05 00:25:34 +0100

    MIC-69 Created annotation for user role validation

---
+++ src/main/java/com/microtech/microtech/security/annotation/RolesAllowed.java
@@ -0,0 +1,12 @@
+package com.microtech.microtech.security.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RolesAllowed {
+    String[] roles();
+}
