a8f3e38ecc645a916f83cadcef73b72626910ad2
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-06 19:00:57 +0100

    MIC-43 Created updateEntityFromDto method signature in productMapper

--- src/main/java/com/microtech/microtech/mapper/ProductMapper.java
+++ src/main/java/com/microtech/microtech/mapper/ProductMapper.java
@@ -1,12 +1,15 @@
 package com.microtech.microtech.mapper;
 
 import com.microtech.microtech.dto.request.product.CreateProductRequest;
+import com.microtech.microtech.dto.request.product.UpdateProductRequest;
 import com.microtech.microtech.dto.response.product.ProductResponse;
 import com.microtech.microtech.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     Product toEntity(CreateProductRequest dto);
     ProductResponse toResponse(Product product);
+    void updateEntityFromDto(UpdateProductRequest dto, @MappingTarget Product product);
 }
