055bd1971ca3a4b92eb1f043cace286022dafe40
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-06 17:30:15 +0100

    MIC-40 Create ProductCreateRequest dto

---
+++ src/main/java/com/microtech/microtech/dto/request/product/CreateProductRequest.java
@@ -0,0 +1,19 @@
+package com.microtech.microtech.dto.request.product;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record CreateProductRequest(
+        @NotBlank(message = "Product name is required!")
+        @Size(min = 3, message = "Product name must be at least 3 characters!")
+        String name,
+        @Min(value = 0, message = "Stock cannot be negative!")
+        int stock,
+        @Positive(message = "Price must be greater than 0!")
+        double price,
+        @Size(min = 10, message = "Description must be at least 10 characters!")
+        String description
+) {
+}
