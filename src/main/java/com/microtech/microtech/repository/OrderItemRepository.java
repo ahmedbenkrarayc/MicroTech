891a1de252624f13d22a3331ddd4989fa13a0cd7
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-06 19:42:22 +0100

    MIC-38 Created OrderItemRepository with existsByProductId

---
+++ src/main/java/com/microtech/microtech/repository/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.microtech.microtech.repository;
+
+import com.microtech.microtech.model.OrderItem;
+import com.microtech.microtech.model.OrderItemKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemKey> {
+    boolean existsByProductId(Long productId);
+}
