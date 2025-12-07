48425e0a748c0de11437e0edb30e9fbd3c702a1b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-07 02:59:14 +0100

    MIC-18 Create updateEntityFromDto in ClientMapper

--- src/main/java/com/microtech/microtech/mapper/ClientMapper.java
+++ src/main/java/com/microtech/microtech/mapper/ClientMapper.java
@@ -1,12 +1,15 @@
 package com.microtech.microtech.mapper;
 
 import com.microtech.microtech.dto.request.auth.CreateClientRequest;
+import com.microtech.microtech.dto.request.auth.UpdateClientRequest;
 import com.microtech.microtech.dto.response.auth.ClientResponse;
 import com.microtech.microtech.model.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
     Client toEntity(CreateClientRequest dto);
     ClientResponse toResponse(Client client);
+    void updateEntityFromDto(UpdateClientRequest dto, @MappingTarget Client client);
 }
